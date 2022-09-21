36b371de9d53adae54de8fcaded391bf3282c57c
yl12053 <qq18929261343@gmail.com>
2022-09-21 14:20:01 +0000

    From REPL: Leaderboard

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
 import flask
+from blueprint import apps
+
 app = flask.Flask(__name__)
-@app.route("/")
-def root():
-  return "Hello World!"
+
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+
+@app.route("/assets/<path:path>")
+def assets(path):
+    return flask.send_from_directory("assets", path)
+
+
+for x in apps:
+    app.register_blueprint(x)
 
 if __name__ == "__main__":
-  app.run("0.0.0.0", 80)
+    app.run("0.0.0.0", 80)
