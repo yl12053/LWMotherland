7de1fdf89ba08f99c9f7995e2e8306f9d3b3fa06
yl12053 <qq18929261343@gmail.com>
2022-09-22 06:14:09 +0000

    From REPL: Create table

--- models/app_db.py
+++ models/app_db.py
@@ -1,2 +1,32 @@
+from flask_sqlalchemy import SQLAlchemy
+
+
+class Model:
+    pass
+
+
 def init(db, app):
+    class_define(db)
     db.init_app(app)
+
+
+def class_define(db):
+
+    class Players(db.Model):
+        __tablename__ = "Players"
+        id = db.Column(db.Integer, primary_key=True)
+        name = db.Column(db.VARCHAR(255))
+        uuid = db.Column(db.VARCHAR(36), unique=True)
+        dim = db.Column(db.Integer)
+        posx = db.Column(db.Integer)
+        posy = db.Column(db.Integer)
+        mark = db.Column(db.Integer)
+
+        def __init__(self, id, name, uuid, dim, posx, posy):
+            self.id = id
+            self.name = name
+            self.uuid = uuid
+            self.dim = dim
+            self.posx = posx
+            self.posy = posy
+            self.mark = mark
