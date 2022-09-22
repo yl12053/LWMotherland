d92ef3fe438f7fa4d808bb3a3041f1acc260c151
yl12053 <qq18929261343@gmail.com>
2022-09-22 12:16:17 +0000

    From REPL: 22/9.20:16

--- app.py
+++ app.py
@@ -5,6 +5,20 @@ from models.app_db import init
 
 app = flask.Flask(__name__)
 
+
+@app.after_request
+def add_header(r):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    r.headers['Cache-Control'] = 'public, max-age=0'
+    return r
+
+
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 db = SQLAlchemy()
@@ -17,6 +31,14 @@ init(db, app)
 def assets(path):
     return flask.send_from_directory("assets", path)
 
+
+@app.route("/")
+def root():
+    return open("index.html", "r").read()
+
+@app.route("/lb")
+def leaderboard():
+    return open("view.html", "r").read()
 
 for x in apps:
     app.register_blueprint(x)
