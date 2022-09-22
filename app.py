fbb385ccf72fcf44513a7976c13b38305fb909fc
yl12053 <qq18929261343@gmail.com>
2022-09-22 06:03:05 +0000

    From REPL: Update database from Azure (mysql)

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 import flask
 from blueprint import apps
+from flask_sqlalchemy import SQLAlchemy
 
 app = flask.Flask(__name__)
 
 app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+db = SQLAlchemy()
+app.config[
+    "SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://lwmotherland%40lwmland:LWm3therland@lwmland.mysql.database.azure.com"
+db.init_app(app)
 
 
 @app.route("/assets/<path:path>")
--- blueprint.py
+++ blueprint.py
@@ -1,3 +1,6 @@
 apps = []
 from leaderboard.app_leader import app
+from apis.app_apis import app as apis
+
 apps.append(app)
+apps.append(apis)
